5ce6676a1a9401306b0652dd7253ab7d4cb6d6d8
Rohit Mathew <rohit.mathew@turtlemint.com>
2019-09-21 20:27:40 +0530

    Updated site content
    Site content has been updated with latest data

--- src/components/Sidebar/Links/index.js
+++ src/components/Sidebar/Links/index.js
@@ -28,6 +28,16 @@ class Links extends Component {
               <i className="fa fa-envelope"></i>
             </a>
           </li>
+          <li className="icon">
+            <a href="https://tinyletter.com/teamix" target="_blank">
+              <i className="fa fa-rss"></i>
+            </a>
+          </li>
+          <li className="icon">
+            <a href="https://open.spotify.com/user/21loszaq56ntdqmategugxofa?si=56YIorANSaeliPYWb5ulUQ" target="_blank">
+              <i className="fa fa-spotify"></i>
+            </a>
+          </li>
         </ul>
         <div className="small">
           Built with ❤️ using <a href="https://www.gatsbyjs.org/" target="_blank">GatsbyJS</a>
