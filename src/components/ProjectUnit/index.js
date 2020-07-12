470d7d47497bdf83354a39ff6f8a509a52e66177
Rohit Mathew <rohit.mathew@turtlemint.com>
2020-07-13 03:26:46 +0530

    Add Projects Section

---
+++ src/components/ProjectUnit/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import './style.scss'
+
+class ProjectUnit extends Component {
+  render() {
+    return (
+      <div className="experience-unit">
+        <a href={this.props.link} target="_blank">
+        <div className="title bold">
+          {this.props.title}
+        </div>
+        </a>
+        <div className="time-period">
+          {this.props.timeperiod}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ProjectUnit
