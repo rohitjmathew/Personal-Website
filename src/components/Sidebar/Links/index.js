160ec061c1add93191824cd57ed8e44c8b4443f4
Rohit Mathew <rohit.mathew@turtlemint.com>
2020-12-16 00:23:03 +0530

    Add dev.to profile

--- src/components/Sidebar/Links/index.js
+++ src/components/Sidebar/Links/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import './style.scss'
-import 'font-awesome/css/font-awesome.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 class Links extends Component {
   render() {
@@ -10,42 +10,42 @@ class Links extends Component {
         <ul className="icons-list">
         <li className="icon">
             <a href="https://www.linkedin.com/in/rohitjmathew/" target="_blank">
-              <i className="fa fa-linkedin" title="rohitjmathew's LinkedIn Profile"></i>
+              <i className="fab fa-linkedin fa-lg" title="rohitjmathew's LinkedIn Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://github.com/rohitjmathew" target="_blank">
-              <i className="fa fa-github" title="rohitjmathew's GitHub Profile"></i>
+              <i className="fab fa-github fa-lg" title="rohitjmathew's GitHub Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://medium.com/@rohitjmathew" target="_blank">
-              <i className="fa fa-medium" title="rohitjmathew's Medium Profile"></i>
+              <i className="fab fa-medium fa-lg" title="rohitjmathew's Medium Profile"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="https://dev.to/rohitjmathew">
-              <i src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" title="rohitjmathew's DEV Profile"></i>
+            <a href="https://dev.to/rohitjmathew" target="_blank">
+              <i className="fab fa-dev fa-lg" title="rohitjmathew's DEV Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="mailto:rohitjmathew@outlook.com" target="_blank">
-              <i className="fa fa-envelope" title="rohitjmathew's Email ID"></i>
+              <i className="fas fa-envelope fa-lg" title="rohitjmathew's Email ID"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://scholar.google.co.in/citations?user=7moht3YAAAAJ&hl=en" target="_blank">
-              <i className="fa fa-graduation-cap" title="rohitjmathew's Google Scholar Profile"></i>
+              <i className="fas fa-graduation-cap fa-lg" title="rohitjmathew's Google Scholar Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://tinyletter.com/teamix" target="_blank">
-              <i className="fa fa-rss" title="rohitjmathew's Music Newsletter"></i>
+              <i className="fas fa-rss fa-lg" title="rohitjmathew's Music Newsletter"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://open.spotify.com/user/21loszaq56ntdqmategugxofa?si=56YIorANSaeliPYWb5ulUQ" target="_blank">
-              <i className="fa fa-spotify" title="rohitjmathew's Spotify Profile"></i>
+              <i className="fab fa-spotify fa-lg" title="rohitjmathew's Spotify Profile"></i>
             </a>
           </li>
         </ul>
