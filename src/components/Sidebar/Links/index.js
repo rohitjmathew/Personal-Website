1309f56c5940aa1b160a7fc007094b81dc75cae0
Rohit Jacob Mathew <rohitjmathew@hotmail.com>
2020-12-15 23:28:56 +0530

    Update index.js

--- src/components/Sidebar/Links/index.js
+++ src/components/Sidebar/Links/index.js
@@ -10,37 +10,42 @@ class Links extends Component {
         <ul className="icons-list">
         <li className="icon">
             <a href="https://www.linkedin.com/in/rohitjmathew/" target="_blank">
-              <i className="fa fa-linkedin"></i>
+              <i className="fa fa-linkedin" title="rohitjmathew's LinkedIn Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://github.com/rohitjmathew" target="_blank">
-              <i className="fa fa-github"></i>
+              <i className="fa fa-github" title="rohitjmathew's GitHub Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://medium.com/@rohitjmathew" target="_blank">
-              <i className="fa fa-medium"></i>
+              <i className="fa fa-medium" title="rohitjmathew's Medium Profile"></i>
+            </a>
+          </li>
+          <li className="icon">
+            <a href="https://dev.to/rohitjmathew">
+              <i className="fab fa-dev" title="rohitjmathew's DEV Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="mailto:rohitjmathew@outlook.com" target="_blank">
-              <i className="fa fa-envelope"></i>
+              <i className="fa fa-envelope" title="rohitjmathew's Email ID"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://scholar.google.co.in/citations?user=7moht3YAAAAJ&hl=en" target="_blank">
-              <i className="fa fa-graduation-cap"></i>
+              <i className="fa fa-graduation-cap" title="rohitjmathew's Google Scholar Profile"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://tinyletter.com/teamix" target="_blank">
-              <i className="fa fa-rss"></i>
+              <i className="fa fa-rss" title="rohitjmathew's Music Newsletter"></i>
             </a>
           </li>
           <li className="icon">
             <a href="https://open.spotify.com/user/21loszaq56ntdqmategugxofa?si=56YIorANSaeliPYWb5ulUQ" target="_blank">
-              <i className="fa fa-spotify"></i>
+              <i className="fa fa-spotify" title="rohitjmathew's Spotify Profile"></i>
             </a>
           </li>
         </ul>
