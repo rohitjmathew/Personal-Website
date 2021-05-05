d6239b6990e6c4648f99deecbede9d1694d8405c
Rohit Mathew <rohit.mathew@turtlemint.com>
2021-05-05 16:04:31 +0530

    Reset Scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,49 +10,48 @@ import Links from '../components/Sidebar/Links'
 import './style.scss'
 
 const IndexPage = () => (
-    <div className="index">
-      <div className="main">
-        <h2>
-          Hi 👋🏽 I'm  <span className="bold">Rohit Jacob Mathew</span>
-        </h2>
+  <div className="index">
+    <div className="main">
+      <h2>
+        Hi 👋🏽 I'm  <span className="bold">Rohit Jacob Mathew</span>
+      </h2>
 
-        <h4>
-          A Senior Software Engineer from India<br/><br/>
+      <h4>
+        A Senior Software Engineer from India<br /><br />
 
-          I am currently working on scaling and building a reliable product for agents to sell insurance at Turtlemint. I am also an Auth0 Ambassador and a former SDE Intern at HackerRank.<br/><br/>
+          I am currently working on scaling and building a reliable product for agents to sell insurance at Turtlemint. I am also an Auth0 Ambassador and a former SDE Intern at HackerRank.<br /><br />
 
           While my interests currently lie in the development of scalable solutions, DevOps, Developer Relations/Dev Evangelism as well as smart AI based system, I'm excited by almost anything in tech.
 
         </h4>
 
-        <br/>
-        <ExperienceSection />
+      <br />
+      <ExperienceSection />
 
-        <br/>
-        <ProjectSection />
-    
-        <br/>
-        <BlogPostSection />
+      <br />
+      <ProjectSection />
 
-        <br/>
-        <ResearchPaperSection />
+      <br />
+      <BlogPostSection />
 
-        <br/>
-        <TechTalkSection />
+      <br />
+      <ResearchPaperSection />
 
-      </div>
+      <br />
+      <TechTalkSection />
 
-      <div className="aside">
-        <div className="top">
-          <About />
-        </div>
-        <div className="bottom">
-          <Links />
-        </div>
-      </div>
-
-      <div class="arrow"></div>
     </div>
+
+    <div className="aside">
+      <div className="top">
+        <About />
+      </div>
+      <div className="bottom">
+        <Links />
+      </div>
+    </div>
+
+  </div>
 )
 
 export default IndexPage
